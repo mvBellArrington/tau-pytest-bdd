9b294392265762cf88b45edd3040220e7cd14041
bellam1 <bellam1@USMFAPX6MN6RQ2>
2021-11-24 08:38:48 -0500

    Adding the missing | character at the end of each examples table

--- tests/features/retirement.feature
+++ tests/features/retirement.feature
@@ -2,13 +2,13 @@
 Feature: Retirement Age Calculator
 	A program that determines retirement age and date based on birth year and birth month.
 
-	@calculate_retirement_age
+	#calculate_retirement_age - invalid cases
 	Scenario: Determine valid year input
 		Given Birth year should be between 1900 and 2021 inclusively
 		When I input 1893
 		Then a ValueError will be thrown
 
-	@calculate_retirement_date
+	#calculate_retirement_date - invalid cases
 	Scenario: Determine valid month input
 		Given The birth year is valid
 		And the birth month should be a numerical value
@@ -17,43 +17,43 @@ Feature: Retirement Age Calculator
 		Then a ValueError is thrown
 
 
-	@calculate_retirement_age
+	#calculate_retirement_age - valid cases
 	Scenario Outline: Calculate retirement age
 		Given the <birth_year> entered is valid
 		When I enter <birth_year>
 		Then the retirement age should be <retirement_age> years as of year 2021
 		And the remaining months of the retirement age should be <remaining_months> months
 		Examples:
-			| birth_year | retirement_age | remaining_months
-			| 1900       | 65	          | 0
-			| 1938       | 65             | 2
-			| 1940       | 65             | 6
-			| 1954       | 66             | 0
-			| 1955       | 66             | 2
-			| 1957       | 66             | 6
-			| 1959       | 66             | 10
-			| 1960       | 67             | 0
-			| 2010       | 67             | 0
+			| birth_year | retirement_age | remaining_months |
+			| 1900       | 65	          | 0                |
+			| 1938       | 65             | 2                |
+			| 1940       | 65             | 6                |
+			| 1954       | 66             | 0                |
+			| 1955       | 66             | 2                |
+			| 1957       | 66             | 6                |
+			| 1959       | 66             | 10               |
+			| 1960       | 67             | 0                |
+			| 2010       | 67             | 0                |
 
-	@calculate_retirement_date
+	#calculate_retirement_date - valid cases
 	Scenario Outline: Calculate month and year of retirement
-		Given the valid birth month is "<birth_month>"
-		And the valid birth year is "<birth_year>"
-		And the retirement age is "<retirement_age>" and "<remaining_months>"
+		Given the valid birth month is <birth_month>
+		And the valid birth year is <birth_year>
+		And the retirement age is <retirement_age> and <remaining_months>
 		When I calculate the retirement date
-		Then the month of retirement should be "<month>" as of 2021
-		And the year of retirement should be "<year>" as of 11/18/2021
+		Then the month of retirement should be <month> as of 2021
+		And the year of retirement should be <year> as of 11/18/2021
 		Examples:
-			| birth_month | birth_year | retirement_age | remaining_months | month | year
-			| 1           | 1900       | 65	            | 0 			   | 1	   | 1965
-			| 2           | 1938       | 65             | 2 	     	   | 4 	   | 2003
-			| 3           | 1940       | 65             | 6           	   | 9 	   | 2005
-			| 4           | 1954       | 66             | 0	               | 4 	   | 2020
-			| 5           | 1955       | 66             | 2                | 7     | 2021
-			| 6           | 1957       | 66             | 6                | 12    | 2023
-			| 7           | 1959       | 66             | 10               | 5     | 2026
-			| 8           | 1960       | 67             | 0                | 8     | 2027
-			| 9           | 2010       | 67             | 0                | 9     | 2077
+			| birth_month | birth_year | retirement_age | remaining_months | month | year |
+			| 1           | 1900       | 65	            | 0 			   | 1	   | 1965 |
+			| 2           | 1938       | 65             | 2 	     	   | 4 	   | 2003 |
+			| 3           | 1940       | 65             | 6           	   | 9 	   | 2005 |
+			| 4           | 1954       | 66             | 0	               | 4 	   | 2020 |
+			| 5           | 1955       | 66             | 2                | 7     | 2021 |
+			| 6           | 1957       | 66             | 6                | 12    | 2023 |
+			| 7           | 1959       | 66             | 10               | 5     | 2026 |
+			| 8           | 1960       | 67             | 0                | 8     | 2027 |
+			| 9           | 2010       | 67             | 0                | 9     | 2077 |
 
 
 
--- tests/step_defs/test_retirement_steps.py
+++ tests/step_defs/test_retirement_steps.py
@@ -10,46 +10,23 @@ CONVERTERS = {
 }
 
 
-@scenario('../features/retirement.feature', 'Calculate retirement age')
-def test_cra():
-    pass
-
-
-#@given(parsers.parse('the <birth_year> entered is valid'), target_fixture="determine_retirement_age")
-@given('the "<birth_year>" entered is valid', target_fixture='determine_retirement_age', converters=CONVERTERS)
-def determine_retirement_age(birth_year):
-    assert calculate_retirement_age(birth_year=birth_year)
-
-
-@when(parsers.parse('I enter <birth_year>'), target_fixture="set_birth_year", converters=CONVERTERS)
-def set_birth_year(birth_year):
-    ra, rm = calculate_retirement_age(birth_year=birth_year)
-    return ra, rm
-
-
-@then(parsers.parse('the retirement age should be "<retirement_age>" years as of year 2021'),
-      target_fixture='execute_retirement_age', converters=CONVERTERS)
-def execute_retirement_age(birth_year, retirement_age):
-    age, months = set_birth_year(birth_year=birth_year)
-    assert age == retirement_age
-
-
-@then(parsers.parse('the remaining months of the retirement age should be "<remaining_months>" months',
-                    target_fixture='execute_retirement_age'))
-def execute_remaining_months(birth_year, remaining_months):
-    age, months = set_birth_year(birth_year=birth_year)
-    assert months == remaining_months
-
-
 @scenario('../features/retirement.feature', 'Calculate month and year of retirement')
-@given(parsers.parse('the valid birth month is "<birth_month>"'))
-@given(parsers.parse('the valid birth yar is "<birth_year>"'))
-@given(parsers.parse('the retirement age is "<retirement_age>" and "<remaining_months>"'))
+@given(parsers.parse('the valid birth month is {birth_month}'),
+       target_fixture='check_all_inputs',
+       converters=CONVERTERS)
+@given(parsers.parse('the valid birth yar is {birth_year}'),
+       target_fixture='check_all_inputs',
+       converters=CONVERTERS)
+@given(parsers.parse('the retirement age is {retirement_age} and {remaining_months}'),
+       target_fixture='check_all_inputs',
+       converters=CONVERTERS)
 def check_all_inputs(birth_month, birth_year, retirement_age, remaining_months):
     assert calculate_retirement_date(birth_year, birth_month, age_years=retirement_age, age_months=remaining_months)
 
 
-@when(parsers.parse('I calculate the retirement date'))
+@when(parsers.parse('I calculate the retirement date'),
+      target_fixture='set_all_inputs',
+      converters=CONVERTERS)
 def set_all_inputs(birth_month, birth_year, retirement_age, remaining_months):
     month, year = calculate_retirement_date(birth_year=birth_year,
                                             birth_month=birth_month,
@@ -58,10 +35,54 @@ def set_all_inputs(birth_month, birth_year, retirement_age, remaining_months):
     return month, year
 
 
-@then(parsers.parse('the month of retirement should be "<month>" as of 2021'))
-@then(parsers.parse('the year of retirement should be "<year>" as of November 18, 2021'))
-def calculate_retirement_date(birth_year, birth_month, retirement_age, remaining_months):
+@then(parsers.parse('the month of retirement should be {month} as of 2021'),
+      target_fixture='calculate_retire_date',
+      converters=CONVERTERS)
+@then(parsers.parse('the year of retirement should be {year} as of November 18, 2021'),
+      target_fixture='calculate_retire_date',
+      converters=CONVERTERS)
+def calculate_retire_date(birth_year, birth_month, retirement_age, remaining_months):
     month, year = set_all_inputs(birth_month, birth_year, retirement_age, remaining_months)
     assert month == month
     assert year == year
+
+
+
+
+@scenario('../features/retirement.feature', 'Calculate retirement age')
+def test_cra():
+    pass
+
+
+@given(parsers.parse('the {birth_year} entered is valid'),
+       target_fixture="determine_retirement_age",
+       converters=CONVERTERS)
+def determine_retirement_age(birth_year):
+    assert calculate_retirement_age(birth_year=birth_year)
+
+
+@when(parsers.parse('I enter {birth_year}'),
+      target_fixture="set_birth_year",
+      converters=CONVERTERS)
+def set_birth_year(birth_year):
+    ra, rm = calculate_retirement_age(birth_year=birth_year)
+    return ra, rm
+
+
+@then(parsers.parse('the retirement age should be {retirement_age} years as of year 2021'),
+      target_fixture='execute_retirement_age',
+      converters=CONVERTERS)
+def execute_retirement_age(birth_year, retirement_age):
+    age, months = set_birth_year(birth_year=birth_year)
+    assert age == retirement_age
+
+
+@then(parsers.parse('the remaining months of the retirement age should be {remaining_months} months'),
+      target_fixture='execute_retirement_age',
+      converters=CONVERTERS)
+def execute_remaining_months(birth_year, remaining_months):
+    age, months = set_birth_year(birth_year=birth_year)
+    assert months == remaining_months
+
+
 
